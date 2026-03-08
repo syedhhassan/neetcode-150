71a8da5ee5648cb06412f229a1cc8f330e933e75
syedhhassan <mbsyedhassan@gmail.com>
2026-03-09 03:50:30 +0530

    287. Find the Duplicate Number

---
+++ Linked-List/find-the-duplicate-number/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast, slow = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow = 0
+        while True:
+            fast = nums[fast]
+            slow = nums[slow]
+            if slow == fast:
+                return slow
