f7822c723e02a5c7f3b4d5c8c7d4df7f3dd0e85f
syedhhassan <mbsyedhassan@gmail.com>
2026-05-29 11:16:31 +0530

    1448. Count Good Nodes in Binary Tree

---
+++ Trees/count-good-nodes-in-binary-tree/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        good = 0
+        stk = [(root, float('-inf'))]
+
+        while stk:
+            node, largest = stk.pop()
+            
+            if largest <= node.val:
+                good += 1
+
+            largest = max(largest, node.val)
+
+            if node.left: stk.append((node.left, largest))
+            if node.right: stk.append((node.right, largest))
+
+        return good
